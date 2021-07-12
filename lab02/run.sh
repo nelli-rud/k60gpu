125e29c399ea792a61a12bd17aac5e16997cd9c3
Nelli-rud <nelli-rud@yandex.ru>
2021-07-12 16:50:44 +0300

    12.07.2021_16:50:44,86

--- lab02/run.sh
+++ lab02/run.sh
@@ -1,8 +1,9 @@
 git pull
 
 mpicxx -O3 -std=c++11 -c main.cpp
+mpicxx -O3 -std=c++11 -c utils.cpp
 
-mpicxx -o myapp2 main.o
+mpicxx -o myapp2 main.o utils.o
 
 mpirun -np 4 ./myapp2
 
---
+++ lab02/utils.cpp
@@ -0,0 +1,49 @@
+#include <mpi.h>
+#include <iostream>
+#include <unistd.h>
+#include <thread>
+#include "utils.h"
+
+using namespace std;
+
+void mpi_init(int argc, char* argv[], int mpi_thread_type, int &provided, int &rank, int &size)
+{
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+    
+    char hostname[50];
+    gethostname(hostname, 50);
+    pid_t pid=getpid();
+    int nHardwareThreads = std::thread::hardware_concurrency();
+    fprintf(stderr, "Hostname: %s. MPI rank: %d. Process ID: %d. Hardware threads: %d \n",
+                      hostname, rank, pid, nHardwareThreads);
+    
+    if  (rank==0) {
+        printf("argc: %d\n", argc);
+        printf("&argc: %p\n", (void*)&argc);
+
+        for(int i=0;i<argc;i++)
+        {
+            printf("argv[%d]: %s\n", i, argv[i]);
+        }
+
+        printf("mpi_thread_type: %d\n", MPI_THREAD_FUNNELED);
+
+        printf("provided: %d\n", provided);
+        printf("&provided: %p\n", &provided);
+
+        printf("size: %d\n", size);
+        printf("&size: %p\n", &size);
+
+        if(provided < MPI_THREAD_FUNNELED)
+        {
+            printf("The threading support level is lesser than that demanded. \n");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+        else
+        {
+            printf("The threading support level: MPI_THREAD_FUNNELED\n");
+        }
+     } 
+}
