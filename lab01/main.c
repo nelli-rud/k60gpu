b53d3f5dba2ee51eb53e873c0deed82e64fa8aba
Nelli-rud <nelli-rud@yandex.ru>
2021-07-12 11:23:14 +0300

    added main.c, vscode.bat

---
+++ lab01/main.c
@@ -0,0 +1,28 @@
+#include <mpi.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+   int rank, size;
+   MPI_Init (&argc, &argv);
+   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+   MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+    if (rank==0)
+       cout << "First MPI program." << "Size is" << size << endl;
+    double t = MPI_Wtime();
+
+    char hostname[50];
+    gethostname(hostname, 50);
+
+    pid_t pid = getpit();
+
+    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. \n", t, hostname, rank, pid);
+
+    MPI_Finalize();
+    return 0;
+
+}
