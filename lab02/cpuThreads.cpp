cd762a25ecefb930a0ad265149689474292a4556
Nelli-rud <nelli-rud@yandex.ru>
2021-07-13 11:10:20 +0300

    13.07.2021_11:10:20,51

--- lab02/cpuThreads.cpp
+++ lab02/cpuThreads.cpp
@@ -26,7 +26,7 @@ fprintf(stderr, "Time: %lf. MPI rank: %d. Process ID: %d. Thread index: %d. paus
 extern "C"
 void testThreads(int rank){
     std::vector<std::thread> threads;
-    for(int i = 0; i < std::thread::hardware_concurrency()-1; i++){
+    for(int i = 0; i < std::thread::hardware_concurrency()-1; i++)
         std::thread thr(thread_proc, i, rank);
         threads.emplace_back(std::move(thr));
     }
